58f83406aa6163bf915a3cb25ef619e30ef393be
Rob Ballou <rob@atendesigngroup.com>
2017-04-07 08:08:03 -0600

    Updating VagrantRunner to correctly build paths

--- src/Runner/VagrantRunner.php
+++ src/Runner/VagrantRunner.php
@@ -4,28 +4,34 @@ namespace Project\Runner;
 use Project\Runner\Runner;
 
 class VagrantRunner extends Runner {
+  /**
+   * Start vagrant.
+   */
   public function run() {
-    $vagrant_directory = $this->validatePath($this->thing->vagrant_directory);
+    $vagrant_directory = $this->validatePath($this->thing->get(['vagrant_directory', 'base']), $this->config);
     if (!$vagrant_directory) {
       throw new \Exception('No vagrant directory specified');
     }
 
     if (!is_dir($vagrant_directory)) {
-      throw new \Exception('No vagrant directory is not a directory');
+      throw new \Exception('The vagrant directory is not a directory: ' . $vagrant_directory);
     }
 
     $ex = $this->getExecutor('cd ' . $vagrant_directory . ' && vagrant up');
     $ex->execute();
   }
 
+  /**
+   * Stop vagrant
+   */
   public function stop() {
-    $vagrant_directory = $this->validatePath($this->thing->vagrant_directory);
+    $vagrant_directory = $this->validatePath($this->thing->get(['vagrant_directory', 'base']), $this->config);
     if (!$vagrant_directory) {
       throw new \Exception('No vagrant directory specified');
     }
 
     if (!is_dir($vagrant_directory)) {
-      throw new \Exception('No vagrant directory is not a directory');
+      throw new \Exception('The vagrant directory is not a directory: ' . $vagrant_directory);
     }
 
     $ex = $this->getExecutor('cd ' . $vagrant_directory . ' && vagrant halt');
